8c90daed42cff1576266ffb2b729cffa4a9184d3
Ivan Steuerberg <irodriguezsteuerberg@danielcastelao.org>
2020-01-31 14:12:43 +0100

    Boletin 18, ejercicio 1

---
+++ Boletin18_1/src/boletin18_1/Boletin18_1.java
@@ -0,0 +1,20 @@
+package boletin18_1;
+
+/**
+ *
+ * @author irodriguezsteuerberg
+ */
+public class Boletin18_1 {
+
+    public static void main(String[] args) {
+
+Metodos obx = new Metodos();
+obx.crearArrayRandom();
+        System.out.println("Array:");
+obx.amosarArray();
+        System.out.println("\nAo reves:");
+obx.amosarArrayReves();
+        
+    }
+    
+}
---
+++ Boletin18_1/src/boletin18_1/Metodos.java
@@ -0,0 +1,31 @@
+package boletin18_1;
+
+/**
+ *
+ * @author irodriguezsteuerberg
+ */
+public class Metodos {
+int[] numeros = new int[6];
+
+public void crearArrayRandom(){
+for (int i = 0; i<numeros.length; i++){
+    numeros[i]=(int)(Math.random()*50+1);
+}
+
+}
+
+public void amosarArray(){
+for (int i = 0 ; i<numeros.length; i++){
+System.out.println(numeros[i]);    
+}
+}
+public void amosarArrayReves(){
+for (int i = numeros.length-1 ; i>=0; i--){
+System.out.println(numeros[i]);    
+}
+}
+
+
+
+
+}
