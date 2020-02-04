714bd31314d07fb7af27ee2fa9bb2dc511f5b5e6
Ivan Steuerberg <irodriguezsteuerberg@danielcastelao.org>
2020-02-04 09:03:42 +0100

    Boletin 18, ejercicio 4

---
+++ Boletin18_4/src/boletin18_4/Boletin18_4.java
@@ -0,0 +1,17 @@
+package boletin18_4;
+
+/**
+ *
+ * @author irodriguezsteuerberg
+ */
+public class Boletin18_4 {
+
+    public static void main(String[] args) {
+Metodos a = new Metodos();
+a.crearArrays();
+a.amosarArrays();
+a.calcularLetra(53196038);
+
+    }
+    
+}
